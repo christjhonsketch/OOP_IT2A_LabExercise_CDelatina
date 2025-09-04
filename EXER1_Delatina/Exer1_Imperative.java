fbcb413f3e2c2542ca05987636bfc214994394ed
Christ Jhon Delatina <christjhon.delatina@dorsu.edu.ph>
2025-09-04 21:37:10 +0800

    third project

---
+++ EXER1_Delatina/Exer1_Imperative.java
@@ -0,0 +1,17 @@
+mport java.util.Arrays;
+import java.util.List;
+
+public class Exer1_Imperative {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        int sumEven = 0;
+        for (int n : numbers) {
+            if (n % 2 == 0) {
+                sumEven += n;
+            }
+        }
+
+        System.out.println("Sum of even numbers (Imperative): " + sumEven);
+    }
+}
